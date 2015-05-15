d3ff79250f58263336a6bca8fa631ffc0fe5d6e7
julianrd <julian.rd7@gmail.com>
2015-05-15 16:50:42 -0300

    verguenza internacional

---
+++ src/main/java/dacs/tpi/App.java
@@ -0,0 +1,13 @@
+package dacs.tpi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
