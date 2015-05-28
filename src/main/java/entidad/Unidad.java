07f8d6063e22041cfa462ebfabc5b045db7f87c2
julianrd <julian.rd7@gmail.com>
2015-05-28 16:42:58 -0300

    Clases Viaje y Unidad Agregadas, Cliente modificada

--- src/main/java/entidad/Unidad.java
+++ src/main/java/entidad/Unidad.java
@@ -1,5 +1,57 @@
 package entidad;
 
-public class Unidad {
+import java.util.Date;
 
+public class Unidad {
+	private String mDominio;
+	private String mModelo;
+	private String mMarca;
+	private Float mCapacidadPeso;
+	private Float mCapacidadTamaño;
+	private Date mFechaAdquisicion;
+	private Viaje mViaje;
+	
+	public String getDominio() {
+		return mDominio;
+	}
+	public void setDominio(String dominio) {
+		mDominio = dominio;
+	}
+	public String getModelo() {
+		return mModelo;
+	}
+	public void setModelo(String modelo) {
+		mModelo = modelo;
+	}
+	public String getMarca() {
+		return mMarca;
+	}
+	public void setMarca(String marca) {
+		mMarca = marca;
+	}
+	public Float getCapacidadPeso() {
+		return mCapacidadPeso;
+	}
+	public void setCapacidadPeso(Float capacidadPeso) {
+		mCapacidadPeso = capacidadPeso;
+	}
+	public Float getCapacidadTamaño() {
+		return mCapacidadTamaño;
+	}
+	public void setCapacidadTamaño(Float capacidadTamaño) {
+		mCapacidadTamaño = capacidadTamaño;
+	}
+	public Date getFechaAdquisicion() {
+		return mFechaAdquisicion;
+	}
+	public void setFechaAdquisicion(Date fechaAdquisicion) {
+		mFechaAdquisicion = fechaAdquisicion;
+	}
+	public Viaje getViaje() {
+		return mViaje;
+	}
+	public void setViaje(Viaje viaje) {
+		mViaje = viaje;
+	}
+	
 }
---
+++ src/main/java/entidad/Viaje.java
@@ -0,0 +1,10 @@
+package entidad;
+
+import java.util.Date;
+
+public class Viaje {
+	private Date mFechaPartida;
+	private Date mFechaLlegadaEstimada;
+	private Date mFechaLlegada;
+	private Estado mEstado;
+}
