4f8a2ed9cdeaa4bcfbba5f523637240fc7f6b572
julianrd <julian.rd7@gmail.com>
2015-05-28 14:52:42 -0300

    cuentenla como quieran loco

---
+++ src/main/java/entidad/Cliente.java
@@ -0,0 +1,19 @@
+package entidad;
+
+public class Cliente {
+	private String mNombre;
+	private int mEdad;
+	
+		public String getNombre() {
+		return mNombre;
+	}
+	public void setNombre(String nombre) {
+		mNombre = nombre;
+	}
+	public int getEdad() {
+		return mEdad;
+	}
+	public void setEdad(int edad) {
+		mEdad = edad;
+	}
+}
