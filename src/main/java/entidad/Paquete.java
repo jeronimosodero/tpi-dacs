84c93f6c91b9d91acb34c2f4f61a204f2db857db
nelsonyaccuzzi <nelsonyaccuzzi@gmail.com>
2015-05-28 17:16:27 -0300

    error en lista de paquetes

--- src/main/java/entidad/Paquete.java
+++ src/main/java/entidad/Paquete.java
@@ -27,12 +27,14 @@ public class Paquete {
 	public void setPeso(float peso) {
 		mPeso = peso;
 	}
-	public Estado getEstado() {
+	
+	public List<Estado> getEstado() {
 		return mEstado;
 	}
-	public void setEstado(Estado estado) {
+	public void setEstado(List<Estado> estado) {
 		mEstado = estado;
 	}
+
 	
 	
 }
