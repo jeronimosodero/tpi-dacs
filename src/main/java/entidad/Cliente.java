56e93e3cec4cbc13ba1a8daad39e69402217031c
julianrd <julian.rd7@gmail.com>
2015-05-28 16:25:14 -0300

    Clase Cliente agregada

--- src/main/java/entidad/Cliente.java
+++ src/main/java/entidad/Cliente.java
@@ -1,19 +1,57 @@
 package entidad;
 
+import java.util.Date;
+
 public class Cliente {
 	private String mNombre;
-	private int mEdad;
+	private Date mFecNac;
+	private String mTelefono;
+	private String mEmail;
+	private int mDNI;
+	private String mCUIL;
+	private Direccion mDireccion;
 	
-		public String getNombre() {
+	public String getNombre() {
 		return mNombre;
 	}
 	public void setNombre(String nombre) {
 		mNombre = nombre;
 	}
-	public int getEdad() {
-		return mEdad;
+	public Date getFecNac() {
+		return mFecNac;
 	}
-	public void setEdad(int edad) {
-		mEdad = edad;
+	public void setFecNac(Date fecNac) {
+		mFecNac = fecNac;
 	}
+	public String getTelefono() {
+		return mTelefono;
+	}
+	public void setTelefono(String telefono) {
+		mTelefono = telefono;
+	}
+	public String getEmail() {
+		return mEmail;
+	}
+	public void setEmail(String email) {
+		mEmail = email;
+	}
+	public int getDNI() {
+		return mDNI;
+	}
+	public void setDNI(int dNI) {
+		mDNI = dNI;
+	}
+	public String getCUIL() {
+		return mCUIL;
+	}
+	public void setCUIL(String cUIL) {
+		mCUIL = cUIL;
+	}
+	public Direccion getDireccion() {
+		return mDireccion;
+	}
+	public void setDireccion(Direccion direccion) {
+		mDireccion = direccion;
+	}
+		
 }
